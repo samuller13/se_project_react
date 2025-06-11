4ea49cabbc558aa8b76b137e7ab0bf53c6b19708
Samantha Muller <samuller13@gmail.com>
2025-06-11 05:25:54 -0700

    add Header and Weather Cards

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
-  return <div>Hello</div>;
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main weatherData={weatherData} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
